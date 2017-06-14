adacd57ac238d8cf5334676724d81f5768963d2c
milad3797 <milad3797@gmail.com>
2017-06-14 15:34:22 +0430

    PleaseWaitFrame class

--- src/ir/aut/view/MyFrame.java
+++ src/ir/aut/view/MyFrame.java
@@ -7,9 +7,5 @@ import javax.swing.*;
  */
 public class MyFrame extends JFrame {
     public static void main(String[] args) {
-        System.out.println("sdddddd");
-        System.out.println("djdlfjsdlfkjsdlfkj");
-        System.out.println("djdlfjsdlfkjsdlfkj");we fewfwfewfwefe
-
     }
 }
