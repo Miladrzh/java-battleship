4cc5430a15399d5edd92bdd228b4ba5863f8abc3
milad3797 <milad3797@gmail.com>
2017-06-15 10:39:01 +0430

    InGameButtonPanel class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
+import ir.aut.view.InGameButtonPanel;
 import ir.aut.view.gameview.GameFrame;
+
+import javax.swing.*;
 
 /**
  * Created by Milad on 6/14/2017.
  */
 public class Main {
     public static void main(String[] args) {
-        GameFrame field = new GameFrame(300 , 300 , 1200 , 1200);
+//        GameFrame field = new GameFrame(300 , 300 , 1200 , 1200);
+        JFrame jFrame = new JFrame("hello");
+        jFrame.add(new InGameButtonPanel("sadra",0,0,400,200));
+        jFrame.setVisible(true);
+        jFrame.setSize(500,300);
     }
 }
