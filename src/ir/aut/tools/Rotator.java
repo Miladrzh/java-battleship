0b25b077506f0aa106261bbe1a1a92da0736ee19
Milad <milad3797@gmail.com>
2017-07-04 11:36:14 +0430

    - changing rotate method

--- src/ir/aut/tools/Rotator.java
+++ src/ir/aut/tools/Rotator.java
@@ -8,10 +8,14 @@ import java.awt.*;
 public class Rotator {
 
     public static Rectangle rotate90(Rectangle r) {
-        int x = r.x , y = r.y , width = r.width , height = r.height;
-        int x2 = x + width, y2 = y + height;
-        int resX = -y, resY = x, resX1 = -y2, resY1 = x2;
-        Rectangle ret = new Rectangle(Math.min(resX, resX1), Math.min(resY, resY1), Math.abs(resX - resX1), Math.abs(resY - resY1));
-        return ret;
+//        int x = r.x , y = r.y , width = r.width , height = r.height;
+//        int x2 = x + width, y2 = y + height;
+//        int resX = -y, resY = x, resX1 = -y2, resY1 = x2;
+//        Rectangle ret = new Rectangle(Math.min(resX, resX1), Math.min(resY, resY1), Math.abs(resX - resX1), Math.abs(resY - resY1));
+//        return ret;
+        int tmp = r.width;
+        r.width = r.height;
+        r.height = tmp;
+        return r;
     }
 }
