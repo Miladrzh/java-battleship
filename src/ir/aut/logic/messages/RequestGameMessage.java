361d2242a1bcfe5c913c4ef0c725d420f73a42eb
Milad <miladrzh@gmail.com>
2017-07-04 16:46:25 +0430

    RequestMessage and feedback message!

--- src/ir/aut/logic/messages/RequestGameMessage.java
+++ src/ir/aut/logic/messages/RequestGameMessage.java
@@ -2,6 +2,8 @@ package ir.aut.logic.messages;
 
 import ir.aut.logic.messages.BaseMessage;
 
+import java.nio.ByteBuffer;
+
 /**
  * Created by Milad on 7/4/2017.
  */
@@ -23,18 +25,41 @@ public class RequestGameMessage extends BaseMessage {
 
     @Override
     protected void serialize() {
+        int ipLength = ip.getBytes().length;
+        int nameLength = name.getBytes().length;
+        int messageLength = 4 + 1 + 1 + 4 + ipLength + 4 + nameLength;
 
+        ByteBuffer byteBuffer = ByteBuffer.allocate(messageLength);
+        byteBuffer.putInt(messageLength);
+        byteBuffer.put(MessageTypes.PROTOCOL_VERSION);
+        byteBuffer.put(MessageTypes.REQUEST_GAME);
+        byteBuffer.putInt(ipLength);
+        byteBuffer.put(ip.getBytes());
+        byteBuffer.putInt(nameLength);
+        byteBuffer.put(name.getBytes());
+        mSerialized = byteBuffer.array();
     }
 
     @Override
     protected void deserialize() {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(mSerialized);
+        int messageLength = byteBuffer.getInt();
+        byte protocolVersion = byteBuffer.get();
+        byte messageType = byteBuffer.get();
 
+        int ipLength = byteBuffer.getInt();
+        byte[] ipByte = new byte[ipLength];
+        byteBuffer.get(ipByte);
+        ip = new String(ipByte);
+
+        int nameLength = byteBuffer.getInt();
+        byte[] nameBytes = new byte[nameLength];
+        byteBuffer.get(nameBytes);
+        name = new String(nameBytes);
     }
 
     @Override
     public byte getMessageType() {
         return (byte)1;
     }
-
-
 }
