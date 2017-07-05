e8c2755bd74f17acf0140f10b888697c56dee800
Milad <miladrzh@gmail.com>
2017-07-05 17:01:47 +0430

    git kharab shode :(

---
+++ src/ir/aut/Game.java
@@ -0,0 +1,12 @@
+package ir.aut;
+
+import ir.aut.view.ConnectionModeFrame;
+
+/**
+ * Created by Milad on 7/5/2017.
+ */
+public class Game {
+    public static void main(String[] args) {
+        ConnectionModeFrame connectionModeFrame = new ConnectionModeFrame();
+    }
+}
