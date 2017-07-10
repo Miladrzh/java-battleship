632f89351d3baf710c6104471c4df031f25c20e8
Milad <milad3797@gmail.com>
2017-07-10 05:26:15 +0430

    fix  youLose bug

--- src/ir/aut/game/Game.java
+++ src/ir/aut/game/Game.java
@@ -191,6 +191,7 @@ public class Game implements ModeFrameCallback, PleaseWaitFrameCallBack, WaitFor
         masterGameFrame.gameFrame.enemySea.setEnableAllCells(true);
         if (hitShips == 20) {
             youWin();
+            messageManager.send(new YouLoseMessage());
         }
         if (!isShip) {
             try {
